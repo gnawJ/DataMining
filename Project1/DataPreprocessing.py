0b7c2bbb48527773e592fbfe9d00b969b823e6ca
Jun <junwang@Juns-MacBook-Air-2.local>
2015-09-12 16:18:48 -0400

    1
    1

--- Project1/DataPreprocessing.py
+++ Project1/DataPreprocessing.py
@@ -1,16 +1,18 @@
 __author__ = 'Po Yao'
 __author__ = 'Jun wang'
 
-import re
+import operator
+import math
 import os, sys
 import nltk
 from nltk.stem.lancaster import LancasterStemmer
 from BeautifulSoup import BeautifulSoup
-
+reload(sys)
+sys.setdefaultencoding('Cp1252')
 # functions go here
 def checkOnlyContainNumAndComma(string):
     flag = True
-    validSet = ['1','2','3','4','5','6','7','8','9','0',',','.']
+    validSet = ['0','2','3','4','5','6','7','8','9','1',',','.']
     for c in string:
         if c not in validSet:
             return False
@@ -23,11 +25,11 @@ def checkOnlyContainNumAndComma(string):
 #   except ValueError:
 #     return False
 
-def buildingIntermediateData1(soup,intermediateData1):
+def buildingIntermediateData1(soup,intermediateData1,totalNumber):
     # iterate every article in the file
     articles = soup.findAll('reuters')
     for article in articles:
-
+        totalNumber += 1
         # find date and use it as key
         dates = article.findAll('date')
         # it should contain only one date else throw exception
@@ -68,7 +70,7 @@ def buildingIntermediateData1(soup,intermediateData1):
                 mutiBodyString += str(body.next.string)
                 bodyDict = buidBodyDict(mutiBodyString)
                 intermediateData1[dateEntry] = [topicsList,placesList,bodyDict]
-    return
+    return totalNumber
 def buidBodyDict(bodyString):
     gabageWords = ['.',',','<','!','@','#','$','%','^','&','*','(',')','_','+','=','?','/','~','`',';',':','>','reut']
     bodyDictionary = {} # {key: word, value: occur times}
@@ -89,11 +91,92 @@ def buidBodyDict(bodyString):
                 bodyDictionary[word] = oldCount + 1
     return bodyDictionary
 
+def buildIntermediateData2AndWordsFreq(intermediateData1, wordsFreq, intermediateData2,IntermediateData3):
+    for key in intermediateData1:
+        words = intermediateData1[key][2]
+        totalWords = 0
+        for word in words:
+            #build 2 and wordsFreq
+            if word not in wordsFreq:
+                wordsFreq[word] = words[word]
+            else:
+                wordsFreq[word] += words[word]
+            if word not in intermediateData2:
+                intermediateData2[word] = 1
+            else:
+                intermediateData2[word] += 1
+            totalWords += words[word]
+        IntermediateData3[key] = totalWords
+def calcTFForEachIntermediateData1(intermediateData1,intermediateData3):
+    TFForEachArticle={}
+    for key in intermediateData1:
+        TFDic = {}
+        words = intermediateData1[key][2]
+        TFValue = 0;
+        for word in words:
+            totalWords = intermediateData3[key]
+            TFValue = words[word]/(totalWords * 1.0)
+            TFDic[word] = TFValue
+        TFForEachArticle[key] = TFDic
+    return TFForEachArticle
+
+def calcIDFForEachWord(totalNumber,intermediateData2):
+    IDF = {}
+    for word in intermediateData2:
+        idfValue = math.log10((totalNumber*1.0)/(1+(intermediateData2[word]*1.0)))
+        IDF[word] = idfValue
+    return  IDF
+def buildTFIDFValueForEachArticle(TF,IDF):
+    TFIDF = {}
+    for article in TF:
+        TFIDFForEachArticle = {}
+        for word in TF[article]:
+            TFIDFValue = TF[article][word] * IDF[word]
+            TFIDFForEachArticle[word] = TFIDFValue
+    TFIDF[article] = TFIDFForEachArticle
+    return TFIDF
+def buidFeatureVectorWay1(TFIDF):
+    featureVec = []
+    for article in TFIDF:
+        sortedValue = sorted(TFIDF[article].values())
+        num = len(sortedValue)
+        max1 = sortedValue[num-1]
+        max2 = sortedValue[num-2]
+        max3 = sortedValue[num-3]
+        attr1 = ''
+        attr2 = ''
+        attr3 = ''
+        for word in TFIDF[article]:
+            
+            #print TFIDF[article][word]
+            if (TFIDF[article][word] == max1 or TFIDF[article][word] == max2 or TFIDF[article][word] == max3) and (attr1 != ''):
+                attri1 = word
+            if (TFIDF[article][word] == max1 or TFIDF[article][word] == max2 or TFIDF[article][word] == max3) and (attr1 != ''):
+                attri2 = word
+            if (TFIDF[article][word] == max1 or TFIDF[article][word] == max2 or TFIDF[article][word] == max3) and (attr1 != ''):
+                attri3 = word
+        featureVec.append(attr1)
+        featureVec.append(attr2)
+        featureVec.append(attr3)
+    return featureVec
+
+
+
 # declare some data structure we need
 # a dictionary structure, whose key is date of each article and value
 # is a list structure {['topics'],['places'], [all the words in each article]}
 intermediateData1 = {}
-
+# a dictionary structure, whose key is date of each word and value
+# is the number of article contains that word. Used for calc IDF
+intermediateData2 = {}
+# used to calc tf
+intermediateData3 = {}
+# tf for each word in each article
+TF = {}
+IDF = {}
+TFIDF ={}
+wordsFreq = {}
+totalNumber = 0
 # read all files in a directory
 # TO Do ask user to type the path of data directory
 path = '/Users/junwang/Desktop/DataMining/Project1/TestDataset/'
@@ -104,8 +187,14 @@ for file in dir:
     sgm = open(path + file).read()
     soup = BeautifulSoup(''.join(sgm))
     # processing raw data step 1: build intermediateData1
-    buildingIntermediateData1(soup,intermediateData1)
-print intermediateData1
+    totalNumber = buildingIntermediateData1(soup,intermediateData1,totalNumber)
+    buildIntermediateData2AndWordsFreq(intermediateData1,wordsFreq,intermediateData2,intermediateData3)
+
+TF = calcTFForEachIntermediateData1(intermediateData1,intermediateData3)
+IDF = calcIDFForEachWord(totalNumber,intermediateData2)
+TFIDF = buildTFIDFValueForEachArticle(TF,IDF)
 
 
+featureVec1 = buidFeatureVectorWay1(TFIDF)
+print featureVec1
 
